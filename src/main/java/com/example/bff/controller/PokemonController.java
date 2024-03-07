502df24650f31871503da7a560e566f768d3e21c
SebassPP <juanpapa@unisabana.edu.co>
2024-03-07 13:33:40 -0500

    Delete Unused imports

--- src/main/java/com/example/bff/controller/PokemonController.java
+++ src/main/java/com/example/bff/controller/PokemonController.java
@@ -1,14 +1,11 @@
 package com.example.bff.controller;
 
 import com.example.bff.service.PokemonServiceImp;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
